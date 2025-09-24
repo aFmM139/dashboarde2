dd2696cf6687f11a88ca8c817af25fef3edf5ee7
andyfajardo13092008 <andyfajardo13092008@gmail.com>
2025-09-24 15:26:56 +0000

    💄ui(IconButton): Añadir sombras al Pressable para estetica

--- components/profile-details.tsx
+++ components/profile-details.tsx
@@ -13,9 +13,9 @@
            <Text className="font-bold text-gray-400">✨ Estudiante ✨</Text>
       
            <View className="flex flex-row gap-4 mt-4">
-            <IconButton icon='instagram' pulsar={()=>console.log("instagram")} color="pink"></IconButton>
-            <IconButton icon='whats-app' pulsar={()=>console.log("whatsapp")} color="green"></IconButton>
-            <IconButton icon='twitter' pulsar={()=>console.log("twitter")} color="blue"></IconButton>
+            <IconButton icon='instagram' pulsar={()=>console.log("instagram")} color= "pink"></IconButton>
+            <IconButton icon='whats-app' pulsar={()=>console.log("whatsapp")} color= "green"></IconButton>
+            <IconButton icon='twitter' pulsar={()=>console.log("twitter")} color= "blue"></IconButton>
             <IconButton icon='tik-tok' pulsar={()=>console.log("tiktok")} ></IconButton>
            </View>
       
